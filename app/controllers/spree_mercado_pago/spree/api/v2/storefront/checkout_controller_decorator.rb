d0ac4cdf400ddb69bf3e7bb2a37ea6c9bd649fec
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2023-05-05 12:21:55 -0300

    fix payment method

--- app/controllers/spree_mercado_pago/spree/api/v2/storefront/checkout_controller_decorator.rb
+++ app/controllers/spree_mercado_pago/spree/api/v2/storefront/checkout_controller_decorator.rb
@@ -10,6 +10,7 @@ module SpreeMercadoPago
 
             def mercadopago_checkout
               pm_id = params[:order][:payments_attributes].first.dig(:payment_method_id)
+              Rails.logger.info ">>>> mercadopago_checkout: #{pm_id}"
               spree_authorize! :update, spree_current_order, order_token
 
               result = update_service.call(
@@ -22,7 +23,7 @@ module SpreeMercadoPago
 
               if spree_current_order.state == 'payment'
                 pm_id = params[:order][:payments_attributes].first.dig(:payment_method_id)
-                payment_method = ::Spree::PaymentMethod.find(pm_id)
+                payment_method = spree_current_order.payments.last&.payment_method
                 if payment_method && payment_method.kind_of?(Spree::PaymentMethod::MercadoPago)
                   # pay with mercadopago
                   payment_response = MercadoPagoCheckout.call(spree_current_order.id, params[:order][:mercadopago])
