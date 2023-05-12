d2cfd4f062a1c5b17881dcbb3851d3bf796ca6bb
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2023-05-12 13:04:40 -0300

    Fix MercadoPagoUpdatePaymentJob

--- app/jobs/mercado_pago_update_payment_job.rb
+++ app/jobs/mercado_pago_update_payment_job.rb
@@ -3,7 +3,7 @@
 class MercadoPagoUpdatePaymentJob < ApplicationJob
   def perform(x_reference, type)
     Rails.logger.info(">>> on MercadoPagoUpdatePaymentJob: #{x_reference}")
-    return if type != "test"
+    return if type == "test"
 
     payment_method = Spree::PaymentMethod.find_by_type("Spree::PaymentMethod::MercadoPago")
     sdk = Mercadopago::SDK.new(payment_method.preferred_access_token)
@@ -12,7 +12,7 @@ class MercadoPagoUpdatePaymentJob < ApplicationJob
     # GET PAYMENT TO CHECK UPDATES
     payment = Spree::Payment.find_by(response_code: x_reference)
     payment ||= Spree::Payment.find_by!(number: x_reference)
-    order = payment.order
+
     if !payment.completed?
       response_message = payment_response.dig("status_detail") || payment_response.dig("message")
       case payment_response.dig("status")
--- lib/spree_mercado_pago/version.rb
+++ lib/spree_mercado_pago/version.rb
@@ -1,5 +1,5 @@
 module SpreeMercadoPago
-  VERSION = '0.1.4'.freeze
+  VERSION = '0.1.5'.freeze
 
   module_function
 
