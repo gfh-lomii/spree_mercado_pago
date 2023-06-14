dc825e152dc9b45f1189d6602204c16694b6f6a8
Chien-Fu <chienfu.udp@gmail.com>
2023-06-14 02:38:10 -0400

    skip unnecesary order update

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -5,36 +5,32 @@ module Spree
     end
 
     def mercadopago_checkout
-      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
-        @order.temporary_address = !params[:save_user_address]
-        if @order.state == 'payment'
-          pm_id = params.dig(:order, :payments_attributes)&.first&.dig(:payment_method_id)
-          payment_method = Spree::PaymentMethod.find(pm_id)
-          if payment_method && payment_method.kind_of?(Spree::PaymentMethod::MercadoPago)
-            # pay with mercadopago
-            payment_response = MercadoPagoCheckout.call(@order.id, params[:order][:mercadopago])
-            @order.reload
-            case payment_response.dig(:status)
-            when "approved"
-              @order.skip_stock_validation = true
-              ix = 0
-              while !@order.completed? && ix < 5
-                @order.next!
-                ix += 1
-              end
+      @order.temporary_address = !params[:save_user_address]
+      return if @order.state != 'payment'
 
-              redirect_to(completion_route) && return
-            when "pending", "in_process"
-              redirect_to(completion_route) && return
-            else
-              flash[:error] = MercadoPagoMessage.call(payment_response.dig(:message))
-              redirect_to(checkout_state_path(@order.state)) && return
-            end
+      pm_id = params.dig(:order, :payments_attributes)&.first&.dig(:payment_method_id)
+      payment_method = Spree::PaymentMethod.find(pm_id)
+      
+      if payment_method && payment_method.kind_of?(Spree::PaymentMethod::MercadoPago)
+        # pay with mercadopago
+        payment_response = MercadoPagoCheckout.call(@order.id, params[:order][:mercadopago])
+        @order.reload
+        case payment_response.dig(:status)
+        when "approved"
+          @order.skip_stock_validation = true
+          ix = 0
+          while !@order.completed? && ix < 5
+            @order.next!
+            ix += 1
           end
+
+          redirect_to(completion_route) && return
+        when "pending", "in_process"
+          redirect_to(completion_route) && return
+        else
+          flash[:error] = MercadoPagoMessage.call(payment_response.dig(:message))
+          redirect_to(checkout_state_path(@order.state)) && return
         end
-      else
-        puts ">>>>> order error message: #{@order.errors.full_messages.join("\n")}"
-        render :edit
       end
     end
   end
--- lib/spree_mercado_pago/version.rb
+++ lib/spree_mercado_pago/version.rb
@@ -1,5 +1,5 @@
 module SpreeMercadoPago
-  VERSION = '0.1.14'.freeze
+  VERSION = '0.1.15'.freeze
 
   module_function
 
